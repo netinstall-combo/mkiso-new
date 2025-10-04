0597726cdc59406361a8489cb063fca908453272
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-10-04 22:03:03 +0000

    build fix

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 mkdir -p work/out
+export DATA=$PWD/data
 export DESTDIR=$PWD/work/out
+export PATH=$PWD/work/out/toolchain/bin:$PATH
 
 for mod in ./src/* ; do
     mkdir -p work/$(basename $mod)
-    if [ ! -f work/$(basename $mod)/done ] ; then
+    if [ ! -f work/$(basename $mod)/done."$1" ] ; then
         echo -e "\033[;32m$mod $1\033[;0m"
         bash -c "cd work/$(basename $mod) ; source ../../$mod ; $1"
-        touch work/$(basename $mod)/done
+        touch work/$(basename $mod)/done."$1"
     fi
 done
