c7d005fd9fa7b1a5a69a47726d763a1483f2d529
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-10-04 20:37:49 +0000

    add busybox

--- src/busybox.sh
+++ src/busybox.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+_bver="1.37.0"
+export CFLAGS="-Wno-all -Os -s"
 function build(){
-    echo "fixme"
+    [ -f busybox_${_bver}.orig.tar.bz2 ] || wget http://deb.debian.org/debian/pool/main/b/busybox/busybox_${_bver}.orig.tar.bz2
+    tar -xf busybox_${_bver}.orig.tar.bz2
+    make defconfig -C busybox-${_bver}
+    sed -i "s|.*CONFIG_STATIC.*|CONFIG_STATIC=y|" busybox-${_bver}/.config
+    sed -i "s|CONFIG_TC=.*|# CONFIG_TC is not set|" busybox-${_bver}/.config
+    yes "" | make oldconfig -j`nproc` -C busybox-${_bver}
+    make -C busybox-${_bver}
 }
 
 function package(){
-    echo "fixme"
+    install busybox-${_bver}/busybox $DESTDIR/busybox
 }
